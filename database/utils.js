b4aebdf1fd158a7b29582eb07e65119347f9e5a2
Daniel Waters <danwater1@gmail.com>
2018-09-21 14:57:46 -0700

    Added pokemon types and type definition table

--- database/utils.js
+++ database/utils.js
@@ -5,16 +5,14 @@ var csv = require('csv-parser')
 
 const rebuildData = () => {
 
-  db.none(`DROP TABLE IF EXISTS public.images`)
-    .then(() => {
-      return db.none('DROP TABLE IF EXISTS public.pokemon')
-    })
-    .then(() => {
-      return db.none('CREATE TABLE public.images (image_id integer PRIMARY KEY, image_path character varying(255))')
-    })
-    .then(() => {
-      return db.none('CREATE TABLE public.pokemon (pokemon_id integer PRIMARY KEY, name character varying(50), image_id integer UNIQUE)');
-    })
+  db.none('DROP TABLE IF EXISTS public.images')
+    .then(() => db.none('DROP TABLE IF EXISTS public.types'))
+    .then(() => db.none('DROP TABLE IF EXISTS public.pokemon_types'))
+    .then(() => db.none('DROP TABLE IF EXISTS public.pokemon'))
+    .then(() => db.none('CREATE TABLE public.pokemon_types (pokemon_id INTEGER, type_id INTEGER, slot INTEGER, CONSTRAINT pokemon_types_pkey PRIMARY KEY (pokemon_id, slot))'))
+    .then(() => db.none('CREATE TABLE public.types (type_id INTEGER PRIMARY KEY, name CHARACTER VARYING(50))'))
+    .then(() => db.none('CREATE TABLE public.pokemon (pokemon_id INTEGER PRIMARY KEY, name CHARACTER VARYING(50), image_id INTEGER UNIQUE)'))
+    .then(() => db.none('CREATE TABLE public.images (image_id INTEGER PRIMARY KEY, image_path CHARACTER VARYING(255))'))
     .then(() => {
       let pokemonData = [];
       fs.createReadStream('./data/csv/pokemon.csv')
@@ -27,10 +25,9 @@ const rebuildData = () => {
           }
         ))
         .on('end', () => {
-          console.log('Done reading pokemone data');
-          db.tx(trans => {
+          db.task(trans => {
             const queries = pokemonData.map(row => {
-              return trans.none('INSERT INTO pokemon(pokemon_id, name, image_id) VALUES($1, $2, $3)', [row.pokemon_id, row.name, row.image_id]);
+              return trans.none('INSERT INTO public.pokemon(pokemon_id, name, image_id) VALUES($1, $2, $3)', [row.pokemon_id, row.name, row.image_id]);
             });
             return trans.batch(queries);
           })
@@ -46,7 +43,7 @@ const rebuildData = () => {
           }
         ))
         .on('end', () => {
-          db.tx(trans => {
+          db.task(trans => {
             const queries = imageData.map(row => {
               return trans.none('INSERT INTO images(image_id, image_path) VALUES($1, $2)', [row.image_id, '/sprites/pokemon/' + row.image_id + '.png']);
             });
@@ -54,8 +51,47 @@ const rebuildData = () => {
           })
         });
     })
+    .then(() => {
+      let typeData = [];
+      fs.createReadStream('./data/csv/types.csv')
+        .pipe(csv())
+        .on('data', data => typeData.push(
+          {
+            type_id: data.type_id,
+            name: data.name
+          }
+        ))
+        .on('end', () => {
+          db.task(trans => {
+            const queries = typeData.map(row => {
+              return trans.none('INSERT INTO types(type_id, name) VALUES($1, $2)', [row.type_id, row.name]);
+            });
+            return trans.batch(queries);
+          })
+        });
+    })
+    .then(() => {
+      let pokemonTypeData = [];
+      fs.createReadStream('./data/csv/pokemon_types.csv')
+        .pipe(csv())
+        .on('data', data => pokemonTypeData.push(
+          {
+            pokemon_id: data.pokemon_id,
+            type_id: data.type_id,
+            slot: data.slot
+          }
+        ))
+        .on('end', () => {
+          db.task(trans => {
+            const queries = pokemonTypeData.map(row => {
+              return trans.none('INSERT INTO pokemon_types(pokemon_id, type_id, slot) VALUES($1, $2, $3)', [row.pokemon_id, row.type_id, row.slot]);
+            });
+            return trans.batch(queries);
+          })
+        });
+    })
     .catch(err => {
-      console.log('Error dropping table with error ' + err)
+      console.log('Error creating database error ' + err)
     });
 }
 
