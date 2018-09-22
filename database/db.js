7e71bd8e8f4b5bbc8d84cf4003e7ab315d697068
Daniel Waters <danwater1@gmail.com>
2018-09-22 08:58:55 -0700

    Adds ESLINT with Google Code Rules

--- database/db.js
+++ database/db.js
@@ -1,16 +1,18 @@
-// Check if connection should be local 
-// or Heroku DB
 const pgp = require('pg-promise')();
 let db;
 
+/* Change database connection based on either using local
+ environment or using Heroku PostGres DB
+ Heroku autocreates the DATABASE_URL environment
+ variable when it runs */
 if (process.env.LOCAL && process.env.LOCAL === 'TRUE') {
   connectionDetails = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS
-  }
+    password: process.env.DB_PASS,
+  };
   db = pgp(connectionDetails);
 } else {
   db = pgp(process.env.DATABASE_URL);
