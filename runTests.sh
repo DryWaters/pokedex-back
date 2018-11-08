3a8814d129b3c3fa341f1a045ed6109f1e5175db
Daniel Waters <danwater1@gmail.com>
2018-11-07 17:08:44 -0800

    Adds redis_url enviornment for redis connection (caching)

--- runTests.sh
+++ runTests.sh
@@ -4,5 +4,6 @@ then
 	head -n 8 .env > .tmp
 fi
 echo -n 'DATABASE_URL=' >> .tmp && heroku config:get DATABASE_URL >> .tmp
+echo -n 'REDIS_URL=' >> .tmp && heroku config:get REDIS_URL >> .tmp
 mv .tmp .env
 qunit
--- watchTests.sh
+++ watchTests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin
 head -n 8 .env > .tmp
 echo -n 'DATABASE_URL=' >> .tmp && heroku config:get DATABASE_URL >> .tmp
+echo -n 'REDIS_URL=' >> .tmp && heroku config:get REDIS_URL >> .tmp
 mv .tmp .env
 qunit -w
