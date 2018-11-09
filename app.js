e5ba583b7cc64d8fa9b52860115d21edc1fdc83b
Daniel Waters <danwater1@gmail.com>
2018-11-08 17:07:18 -0800

    Adds message return when rate limit is exceeded

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.enable('trust proxy'); // enable because using Heroku
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 200, // 200 requests every 15 minutes
+  handler: (req, res) => {
+    const timeTillReset = Math.round(
+        (req.rateLimit.resetTime - new Date()) / 1000);
+    return res.status(429)
+        .json({
+          'error': 'Too many requests.  Please ' +
+            'try again in ' + timeTillReset + ' seconds.'});
+  },
 });
 
 app.use(express.json());
